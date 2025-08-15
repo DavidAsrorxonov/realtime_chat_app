efd4d4bae8f0f7dfd4de8a85e44e57939021ce76
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-16 00:58:42 +0900

    use express json to be able to parse the coming data; create a sign up logic in the controllers;

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,38 @@
-export const signup = (req, res) => {
-  res.send("Sign up route");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long." });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User with this email already exists." });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newuser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newuser) {
+    } else {
+      return res.status(400).json({ message: "Error creating user" });
+    }
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
