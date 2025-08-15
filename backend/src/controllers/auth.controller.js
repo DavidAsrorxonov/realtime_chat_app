1f416b9b24fdcbd88eb0fd10a3c68fd1dcf25f8b
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-16 01:14:23 +0900

    create a function that geenrates json web token and sends it in a cookie to be more secure; import and use the generateToken function in the signup controller; add some field checks before letting users continue; install some necessary packages;

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -9,6 +10,10 @@ export const signup = async (req, res) => {
       return res
         .status(400)
         .json({ message: "Password must be at least 6 characters long." });
+    }
+
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required." });
     }
 
     const user = await User.findOne({ email });
@@ -22,17 +27,30 @@ export const signup = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newuser = new User({
+    const newUser = new User({
       fullName,
       email,
       password: hashedPassword,
     });
 
-    if (newuser) {
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        message: "User created successfully",
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
     } else {
       return res.status(400).json({ message: "Error creating user" });
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error in singup controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const login = (req, res) => {
